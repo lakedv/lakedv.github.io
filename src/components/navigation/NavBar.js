ee053b318f05d8b7e143884adc03916c2aeebe90
lakedv <uaterules@gmail.com>
2024-10-07 13:20:57 -0300

    Updated About/Updated NavBar/Updated Home

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -11,16 +11,16 @@ export default function NavBar() {
 		<nav className='two columns container navbar'>
 			<ul>
 				<li>
-					<Link class='active' to="/"><FontAwesomeIcon icon={faHouseUser} size="3x" /></Link>
+					<Link class='active' to="/"><FontAwesomeIcon icon={faHouseUser} size="3x" /><span className='link-text'>Home</span></Link>
 				</li>
 				<li>
-					<Link to="/Projects"><FontAwesomeIcon icon={faFolderOpen} size="3x" /></Link>
+					<Link to="/Projects"><FontAwesomeIcon icon={faFolderOpen} size="3x" /><span className='link-text'>Projects</span></Link>
 				</li>
 				<li>
-					<Link to="/About"><FontAwesomeIcon icon={faAddressCard} size="3x" /></Link>
+					<Link to="/About"><FontAwesomeIcon icon={faAddressCard} size="3x" /><span className='link-text'>About me</span></Link>
 				</li>
 				<li>
-					<Link to="/Contact"><FontAwesomeIcon icon={faEnvelope} size="3x" /></Link>
+					<Link to="/Contact"><FontAwesomeIcon icon={faEnvelope} size="3x" /><span className='link-text'>Contact me</span></Link>
 				</li>
 			</ul>
 		</nav>
